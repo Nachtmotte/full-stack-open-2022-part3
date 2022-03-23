745585182afd8fc9b256e0f8f95efe3b37a21be4
Nachtmotte <matias_palomeque@hotmail.com>
2022-03-23 17:53:28 -0300

    Exercise 3.21

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === "CastError") {
     return response.status(400).send({ error: "malformatted id" });
+  } else if (error.name === "ValidationError") {
+    return response.status(400).json({ error: error.message });
   }
 
   next(error);
@@ -64,14 +66,6 @@ app.post(
   (request, response, next) => {
     const body = request.body;
 
-    if (!body.name) {
-      return response.status(400).json({ error: "name missing" });
-    }
-
-    if (!body.number) {
-      return response.status(400).json({ error: "number missing" });
-    }
-
     const person = new Person({
       name: body.name,
       number: body.number,
@@ -79,7 +73,7 @@ app.post(
 
     person
       .save()
-      .then((savedPerson) => response.json(savedPerson))
+      .then((savedPerson) => response.json(savedPerson.toJSON()))
       .catch((error) => next(error));
   }
 );
@@ -104,7 +98,7 @@ app.put(
 app.use(unknownEndpoint);
 app.use(errorHandler);
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
