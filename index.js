117153f87dedebd3ac88054d4337f48bd8a04fe8
Nachtmotte <matias_palomeque@hotmail.com>
2022-03-23 15:40:37 -0300

    Exercise 3.18

--- index.js
+++ index.js
@@ -12,6 +12,20 @@ app.use(express.static("build"));
 app.use(cors());
 app.use(express.json());
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: "unknown endpoint" });
+};
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message);
+
+  if (error.name === "CastError") {
+    return response.status(400).send({ error: "malformatted id" });
+  }
+
+  next(error);
+};
+
 app.get("/info", morgan("tiny"), (request, response) => {
   Person.find({}).then((persons) =>
     response.send(
@@ -28,23 +42,26 @@ app.get("/api/persons", morgan("tiny"), (request, response) => {
   });
 });
 
-app.get("/api/persons/:id", morgan("tiny"), (request, response) => {
-  Person.findById(request.params.id).then((person) =>
-    person !== null ? response.json(person) : response.status(404).end()
-  );
+app.get("/api/persons/:id", morgan("tiny"), (request, response, next) => {
+  Person.findById(request.params.id)
+    .then((person) =>
+      person ? response.json(person) : response.status(404).end()
+    )
+    .catch((error) => next(error));
 });
 
-app.delete("/api/persons/:id", morgan("tiny"), (request, response) => {
+app.delete("/api/persons/:id", morgan("tiny"), (request, response, next) => {
   Person.findByIdAndDelete(request.params.id)
     .then((result) =>
-    result !== null ? response.status(200).end() : response.status(404).end())
+      result ? response.status(200).end() : response.status(404).end()
+    )
     .catch((error) => next(error));
 });
 
 app.post(
   "/api/persons",
   morgan(":method :url :status :res[content-length] - :response-time ms :body"),
-  (request, response) => {
+  (request, response, next) => {
     const body = request.body;
 
     if (!body.name) {
@@ -55,19 +72,37 @@ app.post(
       return response.status(400).json({ error: "number missing" });
     }
 
-    const isNameDuplicate = false; //Arreglar
-    if (isNameDuplicate) {
-      return response.status(409).json({ error: "name must be unique" });
-    }
-
     const person = new Person({
       name: body.name,
       number: body.number,
     });
 
-    person.save().then((savedPerson) => response.json(savedPerson));
+    person
+      .save()
+      .then((savedPerson) => response.json(savedPerson))
+      .catch((error) => next(error));
   }
 );
+
+app.put(
+  "/api/persons/:id",
+  morgan(":method :url :status :res[content-length] - :response-time ms :body"),
+  (request, response, next) => {
+    const body = request.body;
+
+    const person = {
+      name: body.name,
+      number: body.number,
+    };
+
+    Person.findByIdAndUpdate(request.params.id, person, { new: true })
+      .then((updatedPerson) => response.json(updatedPerson))
+      .catch((error) => next(error));
+  }
+);
+
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
